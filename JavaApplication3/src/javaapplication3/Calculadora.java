51c7fb570242edb7c4a253af0484eb141ae8cb16
renans.olliveira@gmail.com <renans.olliveira@gmail.com@8095f91a-bbbb-2392-ad40-48b5a164c849>
2014-08-11 15:10:50 +0000

    hue

---
+++ JavaApplication3/src/javaapplication3/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Calculadora {
+ //Atributos 
+    Integer a;
+    Integer b;
+    
+ //Metodos
+    void atribuirA(Integer valor)
+    {
+        a = valor;
+    }
+    void atribuirB(Integer valor)
+    {
+        b = valor;
+    }
+    
+    Integer somar()
+    {
+        return a+b;
+    }
+            
+                
+            
+}
