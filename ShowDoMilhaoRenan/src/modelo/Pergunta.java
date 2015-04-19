44c1c8602ee77f0a75a795867016d0e35a0f68c4
projetorenan <renans.olliveira@gmail.com>
2015-04-19 13:09:30 -0300

    Revert "Melhor"
    This reverts commit cbcc635f277b56e9bfa7e98c73f78c271589ad81.

---
+++ ShowDoMilhaoRenan/src/modelo/Pergunta.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Renan
+ */
+public class Pergunta {
+    
+    private String a, b, c, d, enunciado;
+    private Integer nivel, id; 
+    private String certa; 
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCerta() {
+        return certa;
+    }
+
+    public void setCerta(String certa) {
+        this.certa = certa;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+    
+    
+    
+}
